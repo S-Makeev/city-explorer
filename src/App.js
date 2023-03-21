b8e13b8f5b289c6aa39308c916a39c50d0ff543e
Slava Makeev <unclearnuclear@yahoo.com>
2023-03-21 00:12:33 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pokemonData: [],
+      city: [],
+    }
+  }
+
+  handleCityInput = (event) =>
+  {
+    this.setState({
+      city: event.target.value,
+    })
+  }
+ getCityData = (event) => {
+  event.preventDefault();
+ }
+
+
+  handleGetPokemonData = async (event) => {
+    event.preventDefault();
+
+    //TODO: Use AXIOS TO MAKE A CALL OUT TO POKEMON API
+    let pokemonData = await axios.get('https://pokeapi.co/api/v2/pokemon');
+
+    //TODO: SET DATA INTO STATE
+    this.setState({
+      pokemonData: pokemonData.data.results,
+    })
+
+ 
+  
 }
+    //TODO: RENDER THE DATA
 
+  render() {
+    return (
+      <>
+        <h1>API CALLS</h1>
+        <form>
+          <button type='submit' onClick={this.handleGetPokemonData}>SUBMIT BUTTON</button>
+        </form>
+        <form onsubmit ={this.getCityData}></form>
+
+        <ul>
+          {this.state.pokemonData.map((pokemon, idx) => <li key= {idx}>{pokemon.name}</li>)}
+        </ul>
+      </>
+    )
+  }
+}
 export default App;
