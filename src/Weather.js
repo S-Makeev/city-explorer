5cd272bc488d70b6c33adfabd8ddda0ac203b873
Slava Makeev <71305940+S-Makeev@users.noreply.github.com>
2023-03-24 00:08:19 -0700

    Revert "semi-final"

--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import WeatherDay from './WeatherDay';
+
 
 class Weather extends React.Component {
   render(){
-    
     return(
       <>
         <h3>Daily Weather</h3>
-           
-        {this.props.weatherData.map((forecast, indx) => (
-          <WeatherDay key={indx} forecast={forecast} />
-        ))}
+
+        {this.props.weatherData.map((day, indx) => {
+          return(
+            <>
+            <p>{day.date}</p>
+            <p>{day.description}</p>
+            </>
+          )
+        })}
       </>
     )
   }
